05afe449ebe339f6ecbdac178bd5e0d50ee5227f
jcarpio <jose.carpio@gmail.com>
2025-03-22 23:04:21 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import PrologMobile from "../components/PrologMobile";
+
+export default function Home() {
+  return (
+    <main className="p-4 min-h-screen bg-gray-100">
+      <PrologMobile />
+    </main>
+  );
+}
